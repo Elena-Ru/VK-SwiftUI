7fb01b7947fcd3135db46495e62896a0c6f7a7a3
Elena Russkykh <lenarusskih@gmail.com>
2023-01-02 08:16:33 +0200

    Initial commit

---
+++ VK-SwiftUI/VK-SwiftUI/VK_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VK_SwiftUIApp.swift
+//  VK-SwiftUI
+//
+//  Created by Елена Русских on 02.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct VK_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
