1f2d69716774fa4ff1420f60bd89b6d08fac5149
Elena Russkykh <lenarusskih@gmail.com>
2023-01-04 06:23:26 +0200

    Add "group" image to Assets. Add new file GroupRow.

---
+++ VK-SwiftUI/VK-SwiftUI/GroupRow.swift
@@ -0,0 +1,20 @@
+//
+//  GroupRow.swift
+//  VK-SwiftUI
+//
+//  Created by Елена Русских on 04.01.2023.
+//
+
+import SwiftUI
+
+struct GroupRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GroupRow_Previews: PreviewProvider {
+    static var previews: some View {
+        GroupRow()
+    }
+}
