3f2d6facba01f3754120074a617542d1694ff60e
Elena Russkykh <lenarusskih@gmail.com>
2023-01-06 10:17:41 +0200

    Adopt work Favorite Button

--- VK-SwiftUI/VK-SwiftUI/Friend'sRow.swift
+++ VK-SwiftUI/VK-SwiftUI/Friend'sRow.swift
@@ -8,17 +8,22 @@
 import SwiftUI
 
 struct FriendsRow: View {
-    @State var friend: Friend
+    @EnvironmentObject var modelData: ModelData
+    var friend: Friend
+    
+    var friendIndex: Int {
+           modelData.friends.firstIndex(where: { $0.id == friend.id })!
+       }
     
     var body: some View {
         HStack (spacing: 30){
-            Avatar(avatar: $friend.photo100)
+            Avatar(avatar: $modelData.friends[friendIndex].photo100)
             VStack (alignment: .leading) {
-                NameBoldText(name: $friend.fullName)
-                Secondary2lineText(text: $friend.education)
+                NameBoldText(name: $modelData.friends[friendIndex].fullName)
+                Secondary2lineText(text: $modelData.friends[friendIndex].education)
             }
             Spacer()
-            FavoriteButton(isSet: $friend.isFavorite)
+            FavoriteButton(isSet: $modelData.friends[friendIndex].isFavorite)
         }
         .padding()
     }
@@ -27,7 +32,8 @@ struct FriendsRow: View {
 struct FriendsRow_Previews: PreviewProvider {
     static var friends = ModelData().friends
     static var previews: some View {
-            FriendsRow(friend: friends[0])
+       FriendsRow(friend: friends[0])
+            .environmentObject(ModelData())
     }
 }
 
