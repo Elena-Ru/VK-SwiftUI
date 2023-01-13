57112924074e4c5e7bb568e6b7443056a74ace24
Elena Russkykh <lenarusskih@gmail.com>
2023-01-13 15:14:08 +0200

    Load friends array from Realm

--- VK-SwiftUI/VK-SwiftUI/ViewModel/FriendsViewModel.swift
+++ VK-SwiftUI/VK-SwiftUI/ViewModel/FriendsViewModel.swift
@@ -11,52 +11,55 @@ import RealmSwift
 
 
 class FriendsViewModel: ObservableObject{
+ 
+    @Published var friends: [Friend] = []
+    let realm = try! Realm()
     let baseUrl = "https://api.vk.com"
     let clientId = "51477716" //id_приложения
     
-    @Published var friends: [Friend] = []
-    
     func getFriendsList(token: String, id: Int, completion: @escaping ([Friend]) -> ()){
-
-        let path = "/method/friends.get"
-        let parameters: Parameters = [
-            "access_token" : token,
-            "user_id": id,
-            "client_id": clientId,
-            "fields": "photo_100, education",
-            "v": "5.131"
-        ]
-
-        let url = baseUrl+path
-
-        AF.request(url, method: .post,  parameters: parameters).response { result in
-            if let data = result.data {
-                if let friends = try? JSONDecoder().decode(FriendsResponse.self, from: data).response.items {
-                    DispatchQueue.main.async {
-                        self.friends = friends
-                        self.saveData(friends)
-                        completion(friends)
+        
+        let friendsRealmAr = Array(realm.objects(Friend.self))
+        if !friendsRealmAr.isEmpty {
+            self.friends = friendsRealmAr
+            completion(friends)
+        } else {
+            let path = "/method/friends.get"
+            let parameters: Parameters = [
+                "access_token" : token,
+                "user_id": id,
+                "client_id": clientId,
+                "fields": "photo_100, education",
+                "v": "5.131"
+            ]
+            
+            let url = baseUrl+path
+            
+            AF.request(url, method: .post,  parameters: parameters).response { result in
+                if let data = result.data {
+                    if let friends = try? JSONDecoder().decode(FriendsResponse.self, from: data).response.items {
+                        DispatchQueue.main.async {
+                            self.friends = friends
+                            self.saveData(friends)
+                            completion(friends)
+                        }
                     }
                 }
+                
             }
-
         }
     }
     
     private  func saveData  <T: Object>(_ sData: [T]){
 
-        do{
-            
+        do {
            let realm = try Realm()
             print(realm.configuration.fileURL as Any)
             realm.beginWrite()
             realm.add(sData, update: .all)
             try realm.commitWrite()
-            
         } catch {
-            
             print(error)
-            
         }
       }
 }
